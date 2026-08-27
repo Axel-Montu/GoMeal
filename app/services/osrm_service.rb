87b2b2e16c6fe588bf04905d2a9ad5d614ac64a3
david de-sousa <ddesousa77177@outlook.com>
2026-08-27 14:52:16 +0200

    distance 95% ok

--- app/services/osrm_service.rb
+++ app/services/osrm_service.rb
@@ -3,4 +3,18 @@ class OsrmService
     @user_coordinates = user_coordinates
     @restaurant_coordinates = restaurant_coordinates
   end
+
+  def call
+    url = "https://router.project-osrm.org/route/v1/driving/#{@user_coordinates.join(',')};#{@restaurant_coordinates.join(',')}?overview=false"
+
+    response = Net::HTTP.get(URI(url))
+    data = JSON.parse(response)
+
+    {
+      distance_meters: data["routes"][0]["distance"],
+      distance_km: (data["routes"][0]["distance"] / 1000.0).round(1),
+      duration_seconds: data["routes"][0]["duration"],
+      duration_minutes: (data["routes"][0]["duration"] / 60.0).round
+    }
+  end
 end
