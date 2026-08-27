bb678edeaf78d5e6b3f29ed6aeba8377034c12a7
david de-sousa <ddesousa77177@outlook.com>
2026-08-27 14:31:39 +0200

    distance 40% n2

---
+++ app/services/osrm_service.rb
@@ -0,0 +1,6 @@
+class OsrmService
+  def initialize(user_coordinates, restaurant_coordinates)
+    @user_coordinates = user_coordinates
+    @restaurant_coordinates = restaurant_coordinates
+  end
+end
