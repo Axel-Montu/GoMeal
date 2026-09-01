cafb39326c5059944349684832374be196e0e6d5
romain nicod <ai-gmented@pm.me>
2026-09-01 16:31:42 +0200

    Schema: catch up with the budget column from #102

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_01_104141) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_01_135021) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -79,6 +79,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_01_104141) do
 
   create_table "users", force: :cascade do |t|
     t.integer "average_lunch_time_minutes"
+    t.integer "budget"
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
