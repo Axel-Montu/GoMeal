dee8fce3c1217d50f2cbb9f86b6d68334efccbd6
Baptiste Archimbaud <baptistarchimbaud@gmail.com>
2026-09-03 16:04:04 +0200

    fluid

--- app/javascript/controllers/cuisine_steps_controller.js
+++ app/javascript/controllers/cuisine_steps_controller.js
@@ -4,18 +4,32 @@ export default class extends Controller {
   static targets = ["card"]
 
   connect() {
-    this.showOnly(this.cardTargets[0])
+    this.activate(this.cardTargets[0])
   }
 
   selectBranch(event) {
     event.preventDefault()
     const card = this.cardTargets.find((card) => card.dataset.category === event.params.category)
-    if (card) this.showOnly(card)
+    if (card && card !== this.activeCard) this.transitionTo(card)
   }
 
-  showOnly(cardToShow) {
-    this.cardTargets.forEach((card) => {
-      card.hidden = card !== cardToShow
-    })
+  transitionTo(nextCard) {
+    const previous = this.activeCard
+    if (previous) {
+      previous.classList.remove("cuisine-card--active")
+      previous.classList.add("cuisine-card--leaving")
+      const clear = (event) => {
+        if (event.propertyName !== "opacity") return
+        previous.classList.remove("cuisine-card--leaving")
+        previous.removeEventListener("transitionend", clear)
+      }
+      previous.addEventListener("transitionend", clear)
+    }
+    this.activate(nextCard)
+  }
+
+  activate(card) {
+    card.classList.add("cuisine-card--active")
+    this.activeCard = card
   }
 }
